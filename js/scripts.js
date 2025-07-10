b45b395ddfc5e461f64a5092cf6dd46c9203854b
scamarillo88 <sabrina.camarillo88@gmail.com>
2025-07-10 12:43:35 -0700

    add hamburger menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
 console.log("Hi there! I'm Sabrina, baking sweet designs from scratch - thanks for visiting my portfolio!");
+
+function menuToggle() {
+    const x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
